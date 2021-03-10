ae815e0c6112ef6fd9faccacdd200f3c89a587f7
Hamin <haminjp@naver.com>
2021-03-11 02:29:18 +0900

    feat: chapter 1

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './containers/App';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+// Redux
+import { createStore } from 'redux';
+import reducers from './reducers';
+import { Provider } from 'react-redux';
+
+// Store
+const store = createStore(reducers);
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
