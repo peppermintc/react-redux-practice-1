4ed09ffc85f66c0d3f22c8cf565382f8d4b693a1
Hamin <haminjp@naver.com>
2021-03-11 22:31:23 +0900

    feat: chapter 2

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,14 +1,32 @@
 import React, { Component } from 'react';
-import CounterContainer from './CounterContainer';
+import Buttons from '../components/Buttons';
+import CounterListContainer from './CounterListContainer';
+
+import { connect } from 'react-redux';
+import * as actions from '../actions';
+
+import { getRandomColor } from '../utils';
 
 class App extends Component {
     render() {
+        const { onCreate, onRemove } = this.props;
         return (
-            <div>
-                <CounterContainer />
+            <div className="App">
+                <Buttons 
+                    onCreate={onCreate}
+                    onRemove={onRemove}
+                />
+                <CounterListContainer/>
             </div>
         );
     }
 }
 
-export default App;
+// 액션 함수 준비
+const mapToDispatch = (dispatch) => ({
+    onCreate: () => dispatch(actions.create(getRandomColor())),
+    onRemove: (index) => dispatch(actions.remove(index))
+});
+
+// 리덕스 연결, export
+export default connect(null, mapToDispatch)(App);
--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import reducers from './reducers';
 import { Provider } from 'react-redux';
 
 // Store
-const store = createStore(reducers);
+const store = createStore(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 ReactDOM.render(
   <Provider store={store}>
